2fb9bc2a2d6ede397ec53ac54a70a335dd0e84ed
Hackbright Student <info@hackbrightacademy.com>
2016-07-07 18:03:37 -0700

    renamed input string to tokens

--- calculator.py
+++ calculator.py
@@ -15,40 +15,40 @@ while continue_on == True:
     # Your code goes here
     calculate = raw_input("Please name the operand(+ - * / square cube pow mod) followed by the numbers.")
 
-    input_string = calculate.split(" ")
+    tokens = calculate.split(" ")
 
-    if len (input_string) >= 2:
-        num1 = int(input_string[1])
-    if len(input_string) == 3:
-        num2 = int(input_string[2])
+    if len (tokens) >= 2:
+        num1 = int(tokens[1])
+    if len(tokens) == 3:
+        num2 = int(tokens[2])
     #print input_string
 
-    if input_string[0] == '+':
-        print add(num1,num2)
+    if tokens[0] == '+':
+        print add(num1, num2)
 
-    elif input_string[0] == '-':
-        print subtract(num1,num2)
+    elif tokens[0] == '-':
+        print subtract(num1, num2)
 
-    elif input_string[0] == '*':
-        print multiply(num1,num2)
+    elif tokens[0] == '*':
+        print multiply(num1, num2)
 
-    elif input_string[0] == '/':
+    elif tokens[0] == '/':
         print num1, num2
-        print divide(num1,num2)
+        print divide(num1, num2)
 
-    elif input_string[0] == 'square':
+    elif tokens[0] == 'square':
         print square(num1)
 
-    elif input_string[0] == 'cube':
+    elif tokens[0] == 'cube':
         print cube(num1)
 
-    elif input_string[0] == 'pow':
-        print power(num1,num2)
+    elif tokens[0] == 'pow':
+        print power(num1, num2)
 
-    elif input_string[0] == 'mod':
-        print mod(num1,num2)
+    elif tokens[0] == 'mod':
+        print mod(num1, num2)
 
-    elif input_string[0] == 'q':
+    elif tokens[0] == 'q':
         print "Goodbye"
         continue_on = False
 
